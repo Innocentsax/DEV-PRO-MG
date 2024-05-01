cf136440b748dd26e1523bb6d8200d6bd727007c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-01 20:22:33 +0100

    User service

---
+++ task-user-service/src/main/java/dev/Innocent/TaskUserServiceApplication.java
@@ -0,0 +1,12 @@
+package dev.Innocent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskUserServiceApplication.class, args);
+	}
+}
