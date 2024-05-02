063831cf03eb0187c34b565b04b94946883e1836
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-02 20:56:31 +0100

    Submission Service

---
+++ Submission-Service/src/main/java/dev/Innocent/repository/SubmissionRepository.java
@@ -0,0 +1,10 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.Submission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SubmissionRepository extends JpaRepository<Submission, Long> {
+    List<Submission> findByTaskId(Long taskId);
+}
