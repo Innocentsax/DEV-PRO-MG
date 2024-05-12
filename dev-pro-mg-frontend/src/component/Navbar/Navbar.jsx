fa644ddf3d3aa8c664ed03242f92729904af93c5
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-12 01:19:20 +0100

    Navbar

---
+++ dev-pro-mg-frontend/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div
+      className="container z-10 sticky left-0 right-0 top-0 py-3 px-5 
+    lg:px-10 flex justify-between items-center"
+    >
+      <p className="font-bold text-1g">DEV PRO MANAGER</p>
+
+      <div className="flex items-center gap-5">
+        <p>DEV PRO MG</p>
+        <Avatar src="https://media.istockphoto.com/id/1470505351/photo/portrait-of-a-smiling-doctor-holding-glasses-and-a-mobile-phone-at-the-office.jpg?s=1024x1024&w=is&k=20&c=7qEjVevQHDkL8dPHuwJ3uRVGH4uOOljLhCtSq4vsA-Q=">
+          C
+        </Avatar>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
