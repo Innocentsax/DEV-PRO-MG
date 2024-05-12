1aab7ae822040b9e0a4585e20a7af46fe93cfc96
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-12 01:19:16 +0100

    Navbar

---
+++ dev-pro-mg-frontend/src/theme/darktheme.jsx
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#0c071b",
+    },
+    text: {
+      primary: "fff",
+    },
+    primary: {
+      main: "rgba(215,106,255,0.507",
+    },
+  },
+});
