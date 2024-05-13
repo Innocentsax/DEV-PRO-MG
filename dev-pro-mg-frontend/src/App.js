5070058d44f4420a11c171e23217906baaebf27f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-13 08:16:37 +0100

    Navbar

--- dev-pro-mg-frontend/src/App.js
+++ dev-pro-mg-frontend/src/App.js
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@emotion/react";
 import { darkTheme } from "./theme/darktheme";
 import Navbar from "./component/Navbar/Navbar";
+import Home from "./component/Home/Home";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <Navbar />
+      <Home />
     </ThemeProvider>
   );
 }
