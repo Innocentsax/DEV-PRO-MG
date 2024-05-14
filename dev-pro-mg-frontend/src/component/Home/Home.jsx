fc111a724d62f101edebfaadb3d38e6ccd475238
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-14 08:04:08 +0100

    Home

--- dev-pro-mg-frontend/src/component/Home/Home.jsx
+++ dev-pro-mg-frontend/src/component/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
+import TaskList from "../TaskList/TaskList";
 
 const Home = () => {
   return (
@@ -7,7 +8,9 @@ const Home = () => {
       <div className="hidden lg:block w-[25vw] relative">
         <Sidebar />
       </div>
-      <div className="right-side-part"></div>
+      <div className="right-side-part w-full flex justify-center mb-10">
+        <TaskList />
+      </div>
     </div>
   );
 };
