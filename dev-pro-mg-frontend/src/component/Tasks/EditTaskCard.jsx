09e0309bb4e20a72f2e36963417123f348af1ad6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-16 08:11:48 +0100

    Edit Task

--- dev-pro-mg-frontend/src/component/Tasks/EditTaskCard.jsx
+++ dev-pro-mg-frontend/src/component/Tasks/EditTaskCard.jsx
@@ -1,7 +1,9 @@
-import * as React from "react";
+import { Autocomplete, Button, Grid, TextField } from "@mui/material";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { useState } from "react";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const style = {
   position: "absolute",
@@ -15,7 +17,59 @@ const style = {
   p: 4,
 };
 
+const tags = ["Angular", "React", "Vue.js", "Node.js", "Express", "MongoDB"];
 export default function EditTaskCard({ handleClose, open }) {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    image: "",
+    tags: [],
+    deadline: new Date(),
+  });
+
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleTagsChange = (event, value) => {
+    setSelectedTags(value);
+  };
+
+  const formatedDate = (input) => {
+    let {
+      $y: year,
+      $M: month,
+      $D: day,
+      $H: hours,
+      $m: minutes,
+      $ms: milliseconds,
+    } = input;
+    const date = new Date(year, month, day, hours, minutes, milliseconds);
+    return date.toISOString();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { deadline } = formData;
+    formData.deadline = formatedDate(deadline);
+    formData.tags = selectedTags;
+    console.log("formData", formData, "deadline", formData.deadline);
+    handleClose();
+  };
+
+  const handleDeadlineChange = (date) => {
+    setFormData({
+      ...formData,
+      deadline: date,
+    });
+  };
+
   return (
     <div>
       <Modal
@@ -25,9 +79,71 @@ export default function EditTaskCard({ handleClose, open }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Edit Task Form
-          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2} alignItems={"center"}>
+              <Grid item xs={12}>
+                <TextField
+                  label="Title"
+                  fullWidth
+                  name="title"
+                  value={formData.title}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Image"
+                  fullWidth
+                  name="image"
+                  value={formData.image}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Description"
+                  fullWidth
+                  multiline
+                  rows={4}
+                  name="description"
+                  value={formData.description}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Autocomplete
+                  multiple
+                  id="multiple-limit-tags"
+                  options={tags}
+                  onChange={handleTagsChange}
+                  getOptionLabel={(option) => option}
+                  renderInput={(params) => (
+                    <TextField label="Tags" fullWidth {...params} />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DateTimePicker
+                    onChange={handleDeadlineChange}
+                    className="w-full"
+                    label="Deadline"
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  fullWidth
+                  className="customeButton"
+                  type="submit"
+                  sx={{ padding: ".9rem" }}
+                >
+                  Update
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
         </Box>
       </Modal>
     </div>
