5676ecd45621536150dda8f45336dd0d51636a13
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-16 08:11:25 +0100

    Side Bar

--- dev-pro-mg-frontend/src/component/Sidebar/Sidebar.jsx
+++ dev-pro-mg-frontend/src/component/Sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import { Avatar, Button } from "@mui/material";
 import React from "react";
 import "./Sidebar.css";
 import { useState } from "react";
+import CreateNewTaskForm from "../Tasks/CreateTask";
 
 const menu = [
   { name: "HOME", value: "HOME", role: ["ROLE_ADMIN", "ROLE_CUSTOMER"] },
@@ -15,46 +16,62 @@ const role = "ROLE_ADMIN";
 
 const Sidebar = () => {
   const [activeMenu, setActiveMenu] = useState("DONE");
+  const [openCreateTaskForm, setOpenCreateTaskForm] = useState(false);
+  const handleCloseCreateTaskForm = () => {
+    setOpenCreateTaskForm(false);
+  };
+  const handleOpenCreateTaskModel = () => {
+    setOpenCreateTaskForm(true);
+  };
   const handleMenuChange = (item) => {
+    if (item.name === "CREATE NEW TASK") {
+      handleOpenCreateTaskModel();
+    }
     setActiveMenu(item.name);
   };
   const handleLogout = () => {
     console.log("Handle logout");
   };
   return (
-    <div className="card min-h-[85vh] flex flex-col justify-center fixed w-[20vw]">
-      <div className="space-y-5 h-full">
-        <div className="flex justify-center">
-          <Avatar
-            sx={{ width: "8rem", height: "8rem" }}
-            className="border-2 border-[#2ecc71]"
-            src="https://raw.githubusercontent.com/Innocentsax/DEV-PRO-MG/main/dev-pro-mg-frontend/src/component/Sidebar/logo.png"
-          />
-        </div>
+    <>
+      <div className="card min-h-[85vh] flex flex-col justify-center fixed w-[20vw]">
+        <div className="space-y-5 h-full">
+          <div className="flex justify-center">
+            <Avatar
+              sx={{ width: "8rem", height: "8rem" }}
+              className="border-2 border-[#2ecc71]"
+              src="https://raw.githubusercontent.com/Innocentsax/DEV-PRO-MG/main/dev-pro-mg-frontend/src/component/Sidebar/logo.png"
+            />
+          </div>
 
-        {menu
-          .filter((item) => item.role.includes(role))
-          .map((item) => (
-            <p
-              onClick={() => handleMenuChange(item)}
-              className={`py-3 px-5 rounded-full text-center 
+          {menu
+            .filter((item) => item.role.includes(role))
+            .map((item) => (
+              <p
+                onClick={() => handleMenuChange(item)}
+                className={`py-3 px-5 rounded-full text-center 
             cursor-pointer ${
               activeMenu === item.name ? "activeMenuItem" : "menuItem"
             }`}
-            >
-              {item.name}
-            </p>
-          ))}
-        <Button
-          sx={{ padding: ".7rem", borderRadius: "2rem" }}
-          fullWidth
-          onClick={handleLogout}
-          className="logoutButton"
-        >
-          Logout
-        </Button>
+              >
+                {item.name}
+              </p>
+            ))}
+          <Button
+            sx={{ padding: ".7rem", borderRadius: "2rem" }}
+            fullWidth
+            onClick={handleLogout}
+            className="logoutButton"
+          >
+            Logout
+          </Button>
+        </div>
       </div>
-    </div>
+      <CreateNewTaskForm
+        open={openCreateTaskForm}
+        handleClose={handleCloseCreateTaskForm}
+      />
+    </>
   );
 };
 
