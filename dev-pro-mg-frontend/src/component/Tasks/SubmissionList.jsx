7041af86000de43f304adf920997fb51d6f7e065
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-16 08:12:13 +0100

    Submission

--- dev-pro-mg-frontend/src/component/Tasks/SubmissionList.jsx
+++ dev-pro-mg-frontend/src/component/Tasks/SubmissionList.jsx
@@ -1,20 +1,21 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import SubmissionCard from "./SubmissionCard";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 600,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
+const submissions = [1, 1, 1];
 export default function SubmissionList({ handleClose, open }) {
   return (
     <div>
@@ -25,9 +26,19 @@ export default function SubmissionList({ handleClose, open }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Submission List
-          </Typography>
+          <div>
+            {submissions.length > 0 ? (
+              <div className="space-y-2">
+                {submissions.map((item) => (
+                  <SubmissionCard />
+                ))}
+              </div>
+            ) : (
+              <div className="">
+                <div className="text-center">No Submission Found</div>
+              </div>
+            )}
+          </div>
         </Box>
       </Modal>
     </div>
