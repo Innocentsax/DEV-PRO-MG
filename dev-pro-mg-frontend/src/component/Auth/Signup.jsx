82fec8fd0addb4902d7506a9c2d2fc44e34ee1ae
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-21 09:03:44 +0100

    fix

--- dev-pro-mg-frontend/src/component/Auth/Signup.jsx
+++ dev-pro-mg-frontend/src/component/Auth/Signup.jsx
@@ -1,7 +1,93 @@
-import React from "react";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
 
-const Signup = () => {
-  return <div></div>;
+const Signup = ({ togglePanel }) => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    role: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login form", formData);
+  };
+  return (
+    <div>
+      <h1 className="text-lg font-bold text-center pb-8">Register</h1>
+      <form className="space-y-5" onSubmit={handleSubmit}>
+        <TextField
+          fullWidth
+          label="Full Name"
+          name="fullName"
+          type="fullName"
+          value={formData.fullName}
+          onChange={handleChange}
+          placeholder="enter your Full name ..."
+        />
+        <TextField
+          fullWidth
+          label="Email"
+          name="email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="enter your email ..."
+        />
+        <TextField
+          fullWidth
+          label="Password"
+          name="password"
+          type="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="enter your password ..."
+        />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Role</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={formData.role}
+            label="Role"
+            name="role"
+            onChange={handleChange}
+          >
+            <MenuItem value={"ROLE_CUSTOMER"}>USER</MenuItem>
+            <MenuItem value={"ROLE_ADMIN"}>ADMIN</MenuItem>
+          </Select>
+        </FormControl>
+        <div>
+          <Button
+            fullWidth
+            className="customeButton"
+            type="submit"
+            sx={{ padding: ".9rem" }}
+          >
+            Register
+          </Button>
+        </div>
+      </form>
+      <div>
+        <span>Already have an account?</span>
+        <Button onClick={togglePanel}>Signin</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
