706c01aa73a7a4a8ab6b59f161e18f7bc3cd1294
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-28 13:50:06 +0100

    Store

--- dev-pro-mg-frontend/src/ReduxToolkit/Store.js
+++ dev-pro-mg-frontend/src/ReduxToolkit/Store.js
@@ -1,9 +1,16 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { thunk } from "redux-thunk";
+import AuthReducer from "./AuthSlice";
+import TaskSlice from "./TaskSlice";
+import SubmissionSlice from "./SubmissionSlice";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  auth: AuthReducer,
+});
 const store = configureStore({
   reducer: rootReducer,
+  task: TaskSlice,
+  submission: SubmissionSlice,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
 
