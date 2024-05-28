d28c72f0849a64673b5140a9a4b4a72f45c2c0e4
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-28 19:15:48 +0100

    fix

--- dev-pro-mg-frontend/src/component/TaskList/TaskList.jsx
+++ dev-pro-mg-frontend/src/component/TaskList/TaskList.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import TaskCard from "../Tasks/TaskCard/TaskCard";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchTasks } from "../../ReduxToolkit/TaskSlice";
 
 const TaskList = () => {
+  const dispatch = useDispatch();
+  const { task } = useSelector((store) => store);
+
+  useEffect(() => {
+    dispatch(fetchTasks({}));
+  }, []);
+  console.log("task", task);
   return (
     <div className="w-[67vw]">
       <div className="space-y-5">
