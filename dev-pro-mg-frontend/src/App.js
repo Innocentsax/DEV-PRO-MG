283e9642de3eaef20636cb755df5eba768417d1f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-28 19:15:36 +0100

    fix

--- dev-pro-mg-frontend/src/App.js
+++ dev-pro-mg-frontend/src/App.js
@@ -2,13 +2,25 @@ import { ThemeProvider } from "@emotion/react";
 import { darkTheme } from "./theme/darktheme";
 import Navbar from "./component/Navbar/Navbar";
 import Home from "./component/Home/Home";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchTasks } from "./ReduxToolkit/TaskSlice";
+import { getUserProfile } from "./ReduxToolkit/AuthSlice";
 import Auth from "./component/Auth/Auth";
 
 function App() {
   const user = true;
+  const dispatch = useDispatch();
+  const { task, auth } = useSelector((store) => store);
+
+  useEffect(() => {
+    dispatch(fetchTasks({}));
+    dispatch(getUserProfile(auth.jwt || localStorage.getItem("jwt")));
+  }, [auth.jwt]);
+
   return (
     <ThemeProvider theme={darkTheme}>
-      {user ? (
+      {auth.user ? (
         <div>
           <Navbar />
           <Home />
